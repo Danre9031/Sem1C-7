1fd5169b25b6d2a0450950efcb89735a2eb77943
Mihail <Dante9041@mail.ru>
2022-07-03 19:27:23 +0300

    add dz1

---
+++ DZ1/Program.cs
@@ -0,0 +1,35 @@
+﻿int m = Input("m=");
+int n = Input("n=");
+double[,] number = new double[m, n];
+
+FillSeccondsArrayNumbers(number);
+PrintSeccondArray(number);
+
+void PrintSeccondArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillSeccondsArrayNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = (Convert.ToDouble(new Random().Next(-99, 100)))/10;
+        }
+    }
+}
