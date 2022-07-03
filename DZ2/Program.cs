f035ac972b2180535411ab07906347c4596e09f4
Mihail <Dante9041@mail.ru>
2022-07-03 19:27:38 +0300

    add dz2

---
+++ DZ2/Program.cs
@@ -0,0 +1,56 @@
+﻿int numberСoordinators = Input("Введите координаты числа в массиве в виде двух значного числа=");
+int[,] array = new int[3, 3];
+int lineArray = numberСoordinators / 10;
+int columnArray = numberСoordinators % 2;
+int result=0;
+if (array.GetLength(0) < lineArray || array.GetLength(1) < columnArray)
+{
+    Console.WriteLine($"({lineArray},{columnArray})-> такого чиссла в массиве нет");
+}
+else
+{
+    FillSeccondsArrayRandomsNumbers(array);
+    PrintSeccondArray(array);
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (lineArray == i && columnArray == j)
+            {
+                result=array[i,j];
+                break;
+            }
+
+        }
+    }
+Console.WriteLine($"({lineArray},{columnArray})-> {result}");
+}
+
+void PrintSeccondArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillSeccondsArrayRandomsNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-99, 100);
+        }
+    }
+}
